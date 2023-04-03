628d4f23dcec903ec1ba9893ea0567f5c0650a6d
cristiano <crizatip@gmail.com>
2023-04-03 13:00:45 -0300

    estilização do login e loading

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -3,7 +3,30 @@ import React from 'react';
 class Loading extends React.Component {
   render() {
     return (
-      <div> Carregando... </div>
+      <div className="flex flex-col items-center justify-center text-white">
+        <div
+          className=" m-5 inline-block h-20 w-20 animate-spin rounded-full border-8
+          border-solid border-current border-r-[transparent] align-[-0,125em]
+          motion-reduce:animate-[spin_100s_linear_infinite]"
+          role="status"
+        >
+          <span
+            className="!absolute
+            !-m-2 !h-px !w-px !overflow-hidden
+            !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+          >
+            Loading...
+
+          </span
+          >
+        </div>
+        <span
+          className="font-bold text-xl"
+        >
+          Carregando
+
+        </span>
+      </div>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from './Loading';
+import logo from '../public/music_logo_design_transparent.png';
 
 class Login extends React.Component {
   constructor(props) {
@@ -44,29 +45,48 @@ class Login extends React.Component {
   render() {
     const { nameUser, buttonDisabled, loading, userCreated } = this.state;
     return (
-      <div data-testid="page-login">
+      <div
+        className="flex flex-col justify-center items-center w-screen
+        h-screen bg-[#001822]"
+        data-testid="page-login"
+      >
         {userCreated && <Redirect to="/search" /> }
         {loading && <Loading /> }
         {!loading && (
-          <form id="formLogin" onSubmit={ this.handleSubmit }>
-            <input
-              type="text"
-              data-testid="login-name-input"
-              name="nameUser"
-              id="nameUser"
-              onChange={ this.handleChange }
-              value={ nameUser }
+          <div
+            className="flex flex-col "
+          >
+            <img
+              src={ logo }
+              alt="logo app music"
             />
-            <button
-              type="submit"
-              form="formLogin"
-              data-testid="login-submit-button"
-              disabled={ buttonDisabled }
-              onClick={ this.userValidate }
+            <form
+              id="formLogin"
+              onSubmit={ this.handleSubmit }
             >
-              Entrar
-            </button>
-          </form>
+              <input
+                type="text"
+                className="h-10 w-4/5 rounded-l-lg border-none p-2 text-sm"
+                data-testid="login-name-input"
+                name="nameUser"
+                id="nameUser"
+                placeholder="Seu nome"
+                onChange={ this.handleChange }
+                value={ nameUser }
+              />
+              <button
+                className="rounded-r-lg bg-secondary text-white hover:bg-gradient-to-r
+              hover:from-secondary hover:to-[#00c9c9] p-2"
+                type="submit"
+                form="formLogin"
+                data-testid="login-submit-button"
+                disabled={ buttonDisabled }
+                onClick={ this.userValidate }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
         )}
       </div>
     );
